e5b9b48fb2e1c2a9ea24d98006a2f50dafb5252a
koolwithk <alokmauryaa@gmail.com>
2022-11-02 22:57:12 +0530

    added darwin install and fixed

--- install.sh
+++ install.sh
@@ -6,62 +6,61 @@ if [ "$1" = '--uninstall' ]; then
     exit
 fi
 
-function installed_msg() {
-    echo "Installed kn and kx $tag_name at $HOME/.local/bin/kx $HOME/.local/bin/kn"
-    if [ "$(echo "$PATH" | grep -o "$HOME/.local/bin")" != "$HOME/.local/bin" ]; then
-        echo ''
-        echo '    Please add "$HOME/.local/bin" to your $PATH'
-        echo '    PATH="$PATH:$HOME/.local/bin"'
-        echo ''
-    fi
-}
-
 #### check curl
 if ! [ -x "$(command -v curl)" ]; then
     echo 'Error: curl is not installed.' >&2
     exit 1
 fi
 
+installed_msg() {
+    echo ''
+    echo "Installed kn and kx ${TAG_NAME} at $HOME/.local/bin/kx $HOME/.local/bin/kn"
+        echo ''
+        echo '    Please add "$HOME/.local/bin" to your $PATH'
+        echo '    PATH="$PATH:$HOME/.local/bin"'
+        echo ''
+}
+
+install_kxkn() {
+    TAG_NAME=$1
+    OS=$2
+    ARCH=$3
+    KXKN=$4
+    #kn-v0.1.0-linux-arm64.tar.gz
+    cd /tmp
+    curl -s https://github.com/koolwithk/kx-kn-rust/releases/download/${TAG_NAME}/${KXKN}-${TAG_NAME}-${OS}-${ARCH}.tar.gz -L --output ${KXKN}-${TAG_NAME}-${OS}-${ARCH}.tar.gz
+    tar -xzf ${KXKN}-${TAG_NAME}-${OS}-${ARCH}.tar.gz
+    rm -rf ${KXKN}-${TAG_NAME}-${OS}-${ARCH}.tar.gz
+    mkdir -p $HOME/.local/bin
+    mv ${KXKN} $HOME/.local/bin
+}
+
 #### check Architecture
-if [ "$(uname -m | grep -o aarch64)" = "aarch64" ]; then
+if [ "$(uname -m | grep -o aarch64)" = "aarch64" ] || [ "$(uname -m | grep -o arm64)" = "arm64" ]; then
     arch=arm64
-elif [ "$(uname -m | grep -o arm64)" = "arm64" ]; then
-    arch=arm64
-elif [ "$(uname -m | grep -o x86_64)" = "x86_64" ]; then
+elif [ "$(uname -m | grep -o x86_64)" = "x86_64" ] || [ "$(uname -m | grep -o amd64)" = "amd64" ] ; then
     arch=x86_64
 else
     echo "$(uname -m) $(uname) OS not supported by this script :("
 fi
 
+tag_name=$(curl -s "https://api.github.com/repos/koolwithk/kx-kn-rust/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+
 if [ "$(uname)" = "Linux" ] && [ "x86_64" = "$arch" ] ; then
-    cd /tmp
-    tag_name=$(curl -s "https://api.github.com/repos/koolwithk/kx-kn-rust/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-    curl -s https://github.com/koolwithk/kx-kn-rust/releases/download/$tag_name/kn-$tag_name-x86_64-linux-musl.tar.gz -L --output kn-$tag_name-x86_64-linux-musl.tar.gz
-    tar -xzf kn-$tag_name-x86_64-linux-musl.tar.gz
-    rm -rf kn-$tag_name-x86_64-linux-musl.tar.gz
-    mkdir -p $HOME/.local/bin
-    mv kn $HOME/.local/bin
-
-    curl -s https://github.com/koolwithk/kx-kn-rust/releases/download/$tag_name/kx-$tag_name-x86_64-linux-musl.tar.gz -L --output kx-$tag_name-x86_64-linux-musl.tar.gz
-    tar -xzf kx-$tag_name-x86_64-linux-musl.tar.gz
-    rm -rf kx-$tag_name-x86_64-linux-musl.tar.gz
-    mv kx $HOME/.local/bin
+    os="linux"
+    install_kxkn "$tag_name" "$os" "$arch" "kx"
+    install_kxkn "$tag_name" "$os" "$arch" "kn"
     installed_msg
-
 elif [ "$(uname)" = "Linux" ] && [ "arm64" = "$arch" ] ; then
-    cd /tmp
-    tag_name=$(curl -s "https://api.github.com/repos/koolwithk/kx-kn-rust/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-    curl -s https://github.com/koolwithk/kx-kn-rust/releases/download/$tag_name/kn-$tag_name-arm64-musl-linux.tar.gz -L --output kn-$tag_name-arm64-musl-linux.tar.gz
-    tar -xzf kn-$tag_name-arm64-musl-linux.tar.gz
-    rm -rf kn-$tag_name-arm64-musl-linux.tar.gz
-    mkdir -p $HOME/.local/bin
-    mv kn $HOME/.local/bin
-
-    curl -s https://github.com/koolwithk/kx-kn-rust/releases/download/$tag_name/kx-$tag_name-arm64-musl-linux.tar.gz -L --output kx-$tag_name-arm64-musl-linux.tar.gz
-    tar -xzf kx-$tag_name-arm64-musl-linux.tar.gz
-    rm -rf kx-$tag_name-arm64-musl-linux.tar.gz
-    mv kx $HOME/.local/bin
+    os="linux"
+    install_kxkn "$tag_name" "$os" "$arch" "kx"
+    install_kxkn "$tag_name" "$os" "$arch" "kn"
+    installed_msg
+elif [ "$(uname)" = "Darwin" ] && [ "arm64" = "$arch" ] ; then
+    os="darwin"
+    install_kxkn "$tag_name" "$os" "$arch" "kx"
+    install_kxkn "$tag_name" "$os" "$arch" "kn"
     installed_msg
 else
-    echo "$(uname -m) $(uname) OS not supported by this script :("
+    echo "$(uname -m) $(uname) OS not supported by this script :( Please install from release page."
 fi
