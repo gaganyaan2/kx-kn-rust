041a26c2e6fd1e591fdd4a7f5f8f595df0970244
koolwithk <alokmauryaa@gmail.com>
2022-11-12 22:27:30 +0530

    current-context present check

--- kx/src/kx.rs
+++ kx/src/kx.rs
@@ -8,11 +8,17 @@ use std::process;
 pub fn kx(kubeconfig: &str, kcontext: &str) {
 
     let read_kubeconfig = fs::read_to_string(kubeconfig)
-    .expect("Something went wrong reading the file");
+    .expect("Something went wrong reading KUBECONFIG file");
 
-    let contexts = YamlLoader::load_from_str(&read_kubeconfig).unwrap();
+    let contexts = YamlLoader::load_from_str(&read_kubeconfig).expect("KUBECONFIG file incorrect format");
     let context = &contexts[0];
-    let current_context = context["current-context"].as_str().unwrap();
+    let mut current_context = "";
+
+    //check if current-context present in kubeconfig file
+    if !context["current-context"].as_str().is_none() {
+        current_context = context["current-context"].as_str().unwrap();
+    }
+    
     let all_contexts = &context["contexts"];
     if kcontext == "" {
         // get current context
@@ -26,6 +32,7 @@ pub fn kx(kubeconfig: &str, kcontext: &str) {
         }
 
     } else {
+        // set current context
         let mut check_context_exists = false;
         for context_name in all_contexts.as_vec().unwrap() {
             if context_name["name"].as_str().unwrap() == kcontext {
